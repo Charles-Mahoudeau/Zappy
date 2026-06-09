a46aae5c949bc551b3e6b8f81b1ea43856de6b6f
Noe Caillaud <noe.caillaud@epitech.eu>
2026-06-10 00:41:44 +0200

    feat: main exemple of window usage and cmake fetch raylib

--- zappy/src/zappy/gui/main.cpp
+++ zappy/src/zappy/gui/main.cpp
@@ -10,14 +10,18 @@
 #include <string_view>
 #include <vector>
 
-#include "Demo.hpp"
-#include "zappy/shared/Demo.hpp"
+#include "zappy/gui/display/Window.hpp"
 
 int main(const int argc, char** argv) {
     const std::span argsView{argv, static_cast<std::size_t>(argc)};
     const std::vector<std::string_view> args{argsView.begin(), argsView.end()};
 
-    zappy::gui::hello();
-    zappy::shared::hello();
+    zappy::gui::display::Window window{1600, 900, "Zappy"};
+    window.setTargetFPS(60);
+
+    while (!window.shouldClose()) {
+        window.beginFrame();
+        window.endFrame();
+    }
     return 0;
 }
