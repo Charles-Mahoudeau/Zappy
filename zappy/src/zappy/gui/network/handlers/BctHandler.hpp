cc4ca7e3428137606dc156e149bd31d357108f93
Noe Caillaud <noe.caillaud@epitech.eu>
2026-06-12 15:12:03 +0200

    feat: handler for bct command

---
+++ zappy/src/zappy/gui/network/handlers/BctHandler.hpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** BctHandler
+*/
+
+#pragma once
+
+#include <cstddef>
+#include <functional>
+#include <sstream>
+
+#include "zappy/gui/game/GameState.hpp"
+#include "zappy/gui/network/handlers/ParseHelpers.hpp"
+#include "zappy/shared/exception/ParseException.hpp"
+
+namespace zappy::gui::network::handlers {
+
+class BctHandler {
+  public:
+    explicit BctHandler(game::GameState& state) : _state(state) {}
+
+    void operator()(std::istringstream& ss) {
+        std::size_t x = 0;
+        std::size_t y = 0;
+        if (!(ss >> x >> y)) {
+            throw exception::ParseException{"bct: malformed coordinates"};
+        }
+        _state.get().setTile(x, y, parseResources(ss));
+    }
+
+  private:
+    std::reference_wrapper<game::GameState> _state;
+};
+
+}  // namespace zappy::gui::network::handlers
