00ed2e18b96405ad63fa4e90cd5283d98c49944c
Amos <jhong.almacin@epitech.eu>
2026-06-16 23:26:29 +0200

    feat(Texture): additional constructors

--- zappy/src/zappy/gui/render/Texture.cpp
+++ zappy/src/zappy/gui/render/Texture.cpp
@@ -12,6 +12,17 @@ Texture::Texture(const char* path) { _texture = LoadTexture(path); }
 
 Texture::~Texture() { UnloadTexture(_texture); }
 
+Texture::Texture(Texture&& other) noexcept : _texture(other._texture) { other._texture = {}; }
+
+Texture& Texture::operator=(Texture&& other) noexcept {
+    if (this != &other) {
+        UnloadTexture(_texture);
+        _texture = other._texture;
+        other._texture = {};
+    }
+    return *this;
+}
+
 std::uint32_t Texture::id() const { return _texture.id; }
 
 int Texture::width() const { return _texture.width; }
