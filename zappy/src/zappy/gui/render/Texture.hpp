6b6a4c14b87e7b37e33aa61775ba51f9f0d3cefa
Amos <jhong.almacin@epitech.eu>
2026-06-16 18:41:38 +0200

    feat(Texture): class and basic functions

---
+++ zappy/src/zappy/gui/render/Texture.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2026
+** Zappy
+** File description:
+** Texture
+*/
+
+#pragma once
+
+#include <raylib.h>
+
+#include <cstdint>
+
+namespace zappy::gui::render {
+class Texture {
+    public:
+        Texture(const char* path);
+        ~Texture();
+
+        Texture(const Texture&) = delete;
+        Texture& operator=(const Texture&) = delete;
+
+        Texture(Texture&& other) noexcept;
+        Texture& operator=(Texture&& other) noexcept;
+
+        [[nodiscard]] std::uint32_t id() const;
+        [[nodiscard]] int width() const;
+        [[nodiscard]] int height() const;
+
+    private:
+        Texture2D _texture{};
+};
+}  // namespace zappy::gui::render
