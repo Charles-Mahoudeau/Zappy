4b3c8121403c08b906a1148939b79aac825d13c1
Amos <jhong.almacin@epitech.eu>
2026-06-23 15:36:28 +0200

    refactor(Billboard): rearranged to be more of an object than a placeholder

--- zappy/src/zappy/gui/render/objects/Billboard.hpp
+++ zappy/src/zappy/gui/render/objects/Billboard.hpp
@@ -24,9 +24,6 @@ namespace zappy::gui::render {
 class Billboard : public Texture {
   public:
     explicit Billboard(std::string_view path);
-    Billboard(std::string_view path, Vector3 position, float size, Color tint = WHITE);
-    Billboard(std::string_view path, Rectangle source, Vector3 position, Vector3 up, Vector2 size, Vector2 origin,
-              float rotation, Color tint = WHITE);
     ~Billboard() = default;
 
     Billboard(const Billboard&) = delete;
@@ -34,11 +31,14 @@ class Billboard : public Texture {
     Billboard(Billboard&&) noexcept = default;
     Billboard& operator=(Billboard&&) noexcept = default;
 
+    void setSimpleBillboard(Vector3 position, float size = 1.0F, Color tint = WHITE);
+    void setBillboard(Vector3 position, Vector2 size = {1.0F, 1.0F}, float rotation = 0.0F, Color tint = WHITE,
+                      Vector3 up = {0.0F, 1.0F, 0.0F});
     void setPosition(Vector3 position);
     void setBillboardSize(float size);
     void setSource(Rectangle source);
     void setUp(Vector3 up);
-    void setProSize(Vector2 size);
+    void setSize(Vector2 size);
     void setOrigin(Vector2 origin);
     void setRotation(float rotation);
     void setTint(Color tint);
@@ -47,7 +47,7 @@ class Billboard : public Texture {
     [[nodiscard]] float billboardSize() const;
     [[nodiscard]] Rectangle source() const;
     [[nodiscard]] Vector3 up() const;
-    [[nodiscard]] Vector2 proSize() const;
+    [[nodiscard]] Vector2 size() const;
     [[nodiscard]] Vector2 origin() const;
     [[nodiscard]] float rotation() const;
     [[nodiscard]] Color tint() const;
@@ -57,14 +57,12 @@ class Billboard : public Texture {
 
   private:
     Rectangle _source{};
-    Vector3 _position{};
+    Vector3 _position{.x = 0.0F, .y = 0.0F, .z = 0.0F};
     Vector3 _up{.x = 0.0F, .y = 1.0F, .z = 0.0F};
-    Vector2 _size{.x = 1.0F, .y = 0.0F};
+    Vector2 _size{.x = 1.0F, .y = 1.0F};
     Vector2 _origin{.x = 0.5F, .y = 0.5F};
-    float _billboardSize{1.0F};
     float _rotation{0.0F};
     Color _tint{WHITE};
-    bool _usePro{false};
 };
 
 class BillboardException : public std::runtime_error {
