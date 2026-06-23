52751efbf98de56603ed928ecf7684cf80bf0ef6
Charles Mahoudeau <charles.mahoudeau@epitech.eu>
2026-06-23 23:09:04 +0200

    build(cmake): remove internet check for dependencies

--- zappy/cmake/SetupRaylib.cmake
+++ zappy/cmake/SetupRaylib.cmake
@@ -1,22 +1,15 @@
 include(FetchContent)
-include(InternetCheck)
 
-check_internet_connection()
+FetchContent_Declare(
+        raylib
+        GIT_REPOSITORY https://github.com/raysan5/raylib.git
+        GIT_TAG 6.0
+        FIND_PACKAGE_ARGS NAMES raylib CONFIG
+)
 
-macro(setup_raylib)
-    if (INTERNET_AVAILABLE)
-        FetchContent_Declare(
-                raylib
-                GIT_REPOSITORY https://github.com/raysan5/raylib.git
-                GIT_TAG 6.0
-        )
+set(BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+set(CMAKE_POLICY_VERSION_MINIMUM 3.5)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DSUPPORT_GPU_SKINNING=1" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DSUPPORT_GPU_SKINNING=1" CACHE STRING "" FORCE)
 
-        set(BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
-        set(CMAKE_POLICY_VERSION_MINIMUM 3.5)
-
-        FetchContent_MakeAvailable(raylib)
-        target_compile_definitions(raylib PRIVATE SUPPORT_GPU_SKINNING=1)
-    else ()
-        message(WARNING "Raylib not available: no internet connection.")
-    endif ()
-endmacro()
+FetchContent_MakeAvailable(raylib)
