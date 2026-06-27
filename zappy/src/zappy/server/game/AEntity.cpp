a435459112530e04b349b473ba8d00243184da85
Charles Mahoudeau <charles.mahoudeau@epitech.eu>
2026-06-27 03:33:44 +0200

    refactor(server): pass `Timer` to `AEntity` and expose it via a getter

--- zappy/src/zappy/server/game/AEntity.cpp
+++ zappy/src/zappy/server/game/AEntity.cpp
@@ -19,8 +19,8 @@
 #include "zappy/shared/math/Vector2.hpp"
 
 namespace zappy::server::game {
-AEntity::AEntity(IGrid& grid, IEventEmitter& eventEmitter, std::string teamName)
-    : _grid{grid}, _eventEmitter{eventEmitter}, _teamName{std::move(teamName)} {}
+AEntity::AEntity(Timer& timer, IGrid& grid, IEventEmitter& eventEmitter, std::string teamName)
+    : _timer{timer}, _grid{grid}, _eventEmitter{eventEmitter}, _teamName{std::move(teamName)} {}
 
 std::uint64_t AEntity::id() const { return _id; }
 
@@ -36,6 +36,8 @@ math::Vector2u AEntity::position() const {
 void AEntity::setPosition(const math::Vector2u position) { _grid.get().setPosition(_id, position); }
 
 std::string_view AEntity::teamName() const { return _teamName; }
+
+Timer& AEntity::timer() const { return _timer; }
 
 IEventEmitter& AEntity::eventEmitter() const { return _eventEmitter.get(); }
 }  // namespace zappy::server::game
