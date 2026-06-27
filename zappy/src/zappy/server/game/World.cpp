3fae39f8f74a8e9397141600d1e1153c2b152efd
ewan <ewan.czarny@epitech.eu>
2026-06-27 03:43:11 +0200

    fix (server/command): take/drop TileInventoryEvent missing in behavior

--- zappy/src/zappy/server/game/World.cpp
+++ zappy/src/zappy/server/game/World.cpp
@@ -250,6 +250,7 @@ bool World::playerTake(entity::Player* player, ResourceType resource) {
     tile.inventory().removeResource(resource);
     player->take(resource);
     this->pushEvent(PlayerResourceCollectEvent{.playerId = player->id(), .resourceType = resource});
+    this->pushEvent(TileInventoryEvent(tile.position(), tile.inventory()));
 
     return true;
 }
@@ -262,6 +263,7 @@ bool World::playerDrop(entity::Player* player, ResourceType resource) {
         Tile& tile = this->_grid.tile(player->position());
         tile.inventory().addResource(resource);
         this->pushEvent(PlayerResourceDropEvent{.playerId = player->id(), .resourceType = resource});
+        this->pushEvent(TileInventoryEvent(tile.position(), tile.inventory()));
     } catch (const exception::Exception& err) {
         this->_logger->error(err.what());
         return false;
