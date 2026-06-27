4f6e1b217640152403062e1ba07d3b61af3cd320
Amos <jhong.almacin@epitech.eu>
2026-06-27 19:39:19 +0200

    feat(VFXImpl): Impact vfx v6

--- zappy/src/zappy/gui/render/AssetStore.cpp
+++ zappy/src/zappy/gui/render/AssetStore.cpp
@@ -125,11 +125,11 @@ void AssetStore::loadResourceModel(game::ResourceType type) {
 
 void AssetStore::loadVFXs() {
     ParticleEmitter impact("assets/particles/Impact.png");
-    impact.setStatic(Vector3{}, Vector3{0.5F, 0.5F, 0.5F}, Vector3{0.0F, 0.0F, 0.0F}, 0.0F, 2);
+    impact.setStatic(Vector3{}, Vector3{0.5F, 0.5F, 0.5F}, Vector3{0.0F, 0.0F, 0.0F}, 0.0F, 1);
     impact.setInitParticles(Vector2{0.5F, 0.5F}, 0.0F, ColorF{255.0F, 255.0F, 255.0F, 180.0F}, 45.0F, 0.0F);
     impact.setInitEnvelope(ParticleEmitter::ParticleEnvelope{.size = 0.3F, .rotation = 360.0F, .tint = 0.3F}, 0.1F,
                            0.0F);
-    impact.setIncrementParticles(Vector2{0.03F, 0.03F}, 0.0F, ColorF{0.0F, 0.0F, 0.0F, -1.9F});
+    impact.setIncrementParticles(Vector2{0.03F, 0.03F}, 0.0F, ColorF{0.0F, 0.0F, 0.0F, -4.0F});
     impact.setIncrementEnvelope(ParticleEmitter::ParticleEnvelope{.size = 0.01F, .rotation = 0.02F, .tint = 0.1F});
     _vfxs.insert({"impact", std::move(impact)});
 
