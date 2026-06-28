985bbd9f5363a4b1a67f9c7c8b06a3fde343a803
Amos <jhong.almacin@epitech.eu>
2026-06-28 04:42:43 +0200

    feat(VFXImpl): Food vfx v3

--- zappy/src/zappy/gui/render/objects/particles/VFXHandler.cpp
+++ zappy/src/zappy/gui/render/objects/particles/VFXHandler.cpp
@@ -42,11 +42,11 @@ void VFXHandler::createVFXs() {
     _vfxs.insert({"eggCrack", std::move(eggCrack)});
 
     ParticleEmitter foodDebris("assets/particles/Shard.png");
-    foodDebris.setStatic(Vector3{}, Vector3{2.5F, 2.5F, 2.5F}, Vector3{0.0F, -0.0015F, 0.0F}, 100.0F, 14);
-    foodDebris.setInitParticles(Vector2{0.4F, 0.4F}, 0.0F, ColorF{170.0F, 170.0F, 170.0F, 255.0F}, 90.0F, 0.003F);
-    foodDebris.setInitEnvelope(ParticleEmitter::ParticleEnvelope{.size = 0.2F, .rotation = 360.0F, .tint = 100.0F},
-                               3.0F, 0.03F);
-    foodDebris.setIncrementParticles(Vector2{-0.01F, -0.01F}, 0.0F, ColorF{-1.0F, -1.0F, -1.0F, -1.0F});
+    foodDebris.setStatic(Vector3{}, Vector3{1.5F, 1.5F, 1.5F}, Vector3{0.0F, -0.001F, 0.0F}, 100.0F, 14);
+    foodDebris.setInitParticles(Vector2{0.4F, 0.4F}, 0.0F, ColorF{255.0F, 170.0F, 170.0F, 255.0F}, 90.0F, 0.003F);
+    foodDebris.setInitEnvelope(ParticleEmitter::ParticleEnvelope{.size = 0.2F, .rotation = 360.0F, .tint = 50.0F}, 3.0F,
+                               0.03F);
+    foodDebris.setIncrementParticles(Vector2{-0.007F, -0.007F}, 0.0F, ColorF{-1.0F, -1.0F, -1.0F, -1.0F});
     foodDebris.setIncrementEnvelope(ParticleEmitter::ParticleEnvelope{.size = 0.002F, .rotation = 3.0F, .tint = 0.1F});
     _vfxs.insert({"foodDebris", std::move(foodDebris)});
 
@@ -56,7 +56,7 @@ void VFXHandler::createVFXs() {
     smoke.setInitEnvelope(ParticleEmitter::ParticleEnvelope{.size = 0.2F, .rotation = 360.0F, .tint = 10.0F}, 0.1F,
                           0.03F);
     smoke.setIncrementParticles(Vector2{0.01F, 0.01F}, 0.0F, ColorF{-0.5F, -0.5F, -0.5F, -0.75F});
-    smoke.setIncrementEnvelope(ParticleEmitter::ParticleEnvelope{.size = 0.004F, .rotation = 2.0F, .tint = 0.3F});
+    smoke.setIncrementEnvelope(ParticleEmitter::ParticleEnvelope{.size = 0.004F, .rotation = 20.0F, .tint = 0.3F});
     _vfxs.insert({"smoke", std::move(smoke)});
 
     ParticleEmitter sparkles("assets/particles/Twinkle.png");
